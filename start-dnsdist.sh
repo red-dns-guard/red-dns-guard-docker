1ec698e8d513f9e90c61446f85d8779d3051aacf
resu-cireneg <69966873+resu-cireneg@users.noreply.github.com>
2021-05-01 19:38:35 +0000

    Update start-dnsdist.sh

--- start-dnsdist.sh
+++ start-dnsdist.sh
@@ -11,7 +11,7 @@ cat /etc/rsyslog.conf
 /usr/bin/coredns &
 rsyslogd 
 
-cd /etc/powerdns ;while(true);do  ( sleep 10; while(true);do echo 'showRules()';echo 'showServers()';sleep 360 ;done)|dnsdist --local=0.0.0.0:53   -k "$DNSDISTKEY"  -C /etc/powerdns/dnsdist.lua;sleep 0.2;done &
+cd /etc/powerdns ;while(true);do  ( sleep 10; while(true);do echo 'showRules()';echo 'showServers()';sleep 360 ;done)|dnsdist --local=0.0.0.0:53   -k "$DNSDISTKEY"  -C /etc/powerdns/dnsdist.conf;sleep 0.2;done &
 bash /blocklistgen
 sleep 3 ;bash /WHITE-dnsdist.sh
 
