72d4fe17a3d62d605e17d0515c427e05e1d6237d
M°ŋα Ł¹ſα <commit@only.lan>
2022-11-04 22:17:15 +0100

    2022-11-04-21.17

--- start-dnsdist.sh
+++ start-dnsdist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mylogger()  { while read a;do echo $(date +%Y-%m-%d_%H:%M:%S.%3N -u)" | $1: $a" ;done ; } ;
 test -f /userconfig || mkdir /userconfig
 test -f /userconfig/Corefile && { cat /userconfig/Corefile > /etc/coredns/Corefile ; } ;
 # Check our environment out for a syslog server
@@ -31,17 +31,26 @@ test -e /etc/coredns/Corefile || echo "FAIL::NO COREDNS FILE"
 /usr/bin/coredns -dns.port 55555 -conf /etc/coredns/Corefile ) &
 
 rsyslogd &
-( which tor && tor 2>&1|while read a;do echo $(date +%Y-%m-%d_%H:%M:%S.%3N -u)" | TOR: $a" ;done;sleep 2) &
+( which tor && tor 2>&1|mylogger TOR;sleep 2) &
  
 
-( sleep 10; bash /blocklistgen $REDIS_HOST
+
+( 
+waittime=1;
+server_ready=no;
+waittime=1;while [[ "$server_ready" == "no" ]] ;do ping $REDIS_HOST -c 2 && server_ready=yes;echo waiting "$waittime";sleep $waittime;waittime=$(($waittime*2));done
+
+cd /etc/powerdns ;
+while(true);do
+  ( sleep 10; while(true);do echo 'showRules()';echo 'showServers()';sleep 360 ;done)|dnsdist  -k "$DNSDISTKEY"  -C /etc/powerdns/dnsdist.lua 2>&1 | mylogger DNS
+  sleep 0.2;
+done
+
+
+ping $REDIS_HOST -c2 && 
+ bash /blocklistgen $REDIS_HOST 2>&1 |mylogger BLK
 sleep 130 ;test -e /WHITE-dnsdist.sh && bash /WHITE-dnsdist.sh ) &
 
 
 
-cd /etc/powerdns ;
-while(true);do
-  ( sleep 10; while(true);do echo 'showRules()';echo 'showServers()';sleep 360 ;done)|dnsdist  -k "$DNSDISTKEY"  -C /etc/powerdns/dnsdist.lua;
-  sleep 0.2;
-done
 
