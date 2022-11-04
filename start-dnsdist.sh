5dbbed6b423c2cb208d669c1a10a10f43194dcab
M°ŋα Ł¹ſα <commit@only.lan>
2022-11-04 20:27:08 +0100

    2022-11-04-19.27

--- start-dnsdist.sh
+++ start-dnsdist.sh
@@ -8,22 +8,26 @@ test -f /userconfig/Corefile && { cat /userconfig/Corefile > /etc/coredns/Corefi
 [[ -z "${REDIS_HOST}" ]] && REDIS_HOST=redis
 
 mkdir -p /var/spool/rsyslog
-if [ ! -z ${SYSLOG_HOST+x} ]; then
-    cat >> /etc/rsyslog.conf << EOF
-    *.* @$SYSLOG_HOST:514
-EOF
+if [ ! -z ${SYSLOG_HOST} ]; then
+echo "*.* @$SYSLOG_HOST:514" >  /etc/rsyslog.conf 
+
+#    cat >> /etc/rsyslog.conf << EOF
+#    *.* @$SYSLOG_HOST:514
+#EOF
 
 fi
 
 [[ -z "$DNSDISTKEY" ]] && DNSDISTKEY=$(for rounds in $(seq 1 24);do cat /dev/urandom |tr -cd '[:alnum:]_\-.'  |head -c48;echo ;done|grep -e "_" -e "\-" -e "\."|grep ^[a-zA-Z0-9]|grep [a-zA-Z0-9]$|tail -n1)
 cat /etc/rsyslog.conf
-mkdir -p /etc/coredns/hosts/alternates/fakenews-gambling/
+
+( mkdir -p /etc/coredns/hosts/alternates/fakenews-gambling/
 wget -c "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts" -O /etc/coredns/hosts/alternates/fakenews-gambling/hosts
 test -f /etc/coredns/hosts/alternates/fakenews-gambling/hosts || { echo > /etc/coredns/hosts/alternates/fakenews-gambling/hosts ; } ;
 
-/usr/bin/coredns -dns.port 55555 -conf /etc/coredns/Corefile &
+/usr/bin/coredns -dns.port 55555 -conf /etc/coredns/Corefile ) &
+
 rsyslogd &
-which tor && /etc/init.d/tor start
+( which tor && /etc/init.d/tor start ) & 
 
 ( sleep 10; bash /blocklistgen
 sleep 130 ;bash /WHITE-dnsdist.sh ) &
